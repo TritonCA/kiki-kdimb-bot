b619f836bdd646d88b80a7e63f848093d8c52b80
Idioma Software <ruslansuhoguzov@gmail.com>
2026-06-06 16:07:08 +0300

    Исправлена ошибка импорта kiki.py

--- kdimb_bot/kiki.py
+++ kdimb_bot/kiki.py
@@ -2,12 +2,12 @@ import os
 import telebot
 from threading import Thread
 
-from flask_support.flask_sup import flask_thread
+from kdimb_bot.flask_support.flask_sup import flask_thread
 
-from core import *
+from kdimb_bot.core import *
 
-from questions.strategies.antispam import register_handlers as register_antispam
-from questions.strategies.quiz import register_handlers as register_quiz
+from kdimb_bot.questions.strategies.antispam import register_handlers as register_antispam
+from kdimb_bot.questions.strategies.quiz import register_handlers as register_quiz
 
 START_MESSAGE = "Я Кики. Меня потеряли, ваша группа меня ищет? Добавьте меня туда. (Уверяю, мне можно доверить права администратора)"
 
